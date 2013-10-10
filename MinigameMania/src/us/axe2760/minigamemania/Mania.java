4d59191d5ed4c4f27e19edf8bba685c75655f8ab
Axe2760 <null>
2013-10-10 15:50:10 -0500

    Started basic stuff.

--- MinigameMania/src/us/axe2760/minigamemania/Mania.java
+++ MinigameMania/src/us/axe2760/minigamemania/Mania.java
@@ -1,5 +1,24 @@
 package us.axe2760.minigamemania;
 
-public class Mania {
+import java.io.File;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mania extends JavaPlugin{
+	
+	public void onEnable(){
+		
+		//load from the minigames folder
+		
+		if (!this.getDataFolder().exists()){
+			this.getDataFolder().mkdir();
+		}
+		
+		File minigameFolder = new File(this.getDataFolder(), "minigames");
+		
+		if (!minigameFolder.exists()){
+			minigameFolder.mkdir();
+		}
+	}
+	
 }
